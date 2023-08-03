05f92109fcdb7ee2bc79b3d31a1ebbd4026b4344
CHOIGOYO <choigoyo.q7@gmail.com>
2023-08-03 15:15:47 +0900

    user model

--- lib/main.dart
+++ lib/main.dart
@@ -21,12 +21,34 @@ class JSonTest extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String json = '{"name":"choigoyo","email":"choigoyo.q7@gmail.com","created_time":"2023-08-03"}';
+    String json = '{"name":"최고요","email":"choigoyo.q7@gmail.com","created_time":20230000}';
     Map<String, dynamic> userMap = jsonDecode(json);
+    var user = User.fromJson(userMap);
     return Scaffold(
       body: Center(
-        child: Text(' name : ${userMap['name']} \n email : ${userMap['email']} \n created_time : ${userMap['created_time']} \n '),
+        child: Text(' name : ${user.name} \n email : ${user.email} \n created_time : ${user.createdTime} \n '),
       ),
     );
   }
+}
+
+// model
+class User{
+  final String name;
+  final String email;
+  final int createdTime;
+
+  User(this.name, this.email, this.createdTime);
+
+  User.fromJson(Map<String, dynamic> json)
+      : name = json['name'],
+        email = json['email'],
+        createdTime = json['created_time'];
+
+  Map<String, dynamic> toJson() => {
+    'name': name,
+    'email': email,
+    'created_time': createdTime,
+  };
+
 }
